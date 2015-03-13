3a75e995b116514df081b6f1b263bb4a5aa60600
Gautier Delplace <gautier.delplace@hotmail.fr>
2015-03-13 13:41:18 +0100

    Rajout + reglage taille carte

--- JeuDeCarte/src/Carte.java
+++ JeuDeCarte/src/Carte.java
@@ -1,4 +1,5 @@
 /*http://commons.wikimedia.org/wiki/Playing_card
+ * Pour jeu de 32 ou 52 carte
  * code image : 
  * Valeur : 1 -> 13 == AS->ROI
  * Couleur : 1 == coeur, 2 == carreau, 3 == trefle, 4 == pique
