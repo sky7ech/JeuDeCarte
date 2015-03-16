84bd106dc512c459962220fea4e21a61d8e421e3
Gautier Delplace <gautier.delplace@hotmail.fr>
2015-03-16 09:20:57 +0100

    Coté JavaScript: page de jeu d'une partie de poker

---
+++ JeuDeCarte/Web/JavaScript/TirageCarte.js
@@ -0,0 +1,38 @@
+function tirageAleatoire(){
+	var couleur1 = Math.floor(Math.random() *4) +1;
+	var type1 = Math.floor(Math.random() * 13) +1;
+	$("#Carte1").append("<img src=\"../CSS/Image/"+type1 + "_" + couleur1 + ".jpg\" alt=\"...\" class=\"img-rounded\" height=\"120\" width=\"87\">");
+	do{
+		couleur2 = Math.floor(Math.random() *4) +1;
+		type2 = Math.floor(Math.random() * 13) +1;
+	}
+	while(couleur1 == couleur2 && type1==type2)
+	
+	$("#Carte2").append("<img src=\"../CSS/Image/"+type2 + "_" + couleur2 + ".jpg\" alt=\"...\" class=\"img-rounded\" height=\"120\" width=\"87\">");
+}
+tirageAleatoire();
+
+
+Flop.onclick=function(){
+	var couleur1 = Math.floor(Math.random() *4) +1;
+	var type1 = Math.floor(Math.random() * 13) +1;
+	$("#Flop1").html("<img src=\"../CSS/Image/"+type1 + "_" + couleur1 + ".jpg\" alt=\"...\" class=\"img-rounded\" height=\"120\" width=\"87\">");
+	var couleur1 = Math.floor(Math.random() *4) +1;
+	var type1 = Math.floor(Math.random() * 13) +1;
+	$("#Flop2").html("<img src=\"../CSS/Image/"+type1 + "_" + couleur1 + ".jpg\" alt=\"...\" class=\"img-rounded\" height=\"120\" width=\"87\">");
+	var couleur1 = Math.floor(Math.random() *4) +1;
+	var type1 = Math.floor(Math.random() * 13) +1;
+	$("#Flop3").html("<img src=\"../CSS/Image/"+type1 + "_" + couleur1 + ".jpg\" alt=\"...\" class=\"img-rounded\" height=\"120\" width=\"87\">");
+}
+
+Turn1.onclick=function(){
+	var couleur1 = Math.floor(Math.random() *4) +1;
+	var type1 = Math.floor(Math.random() * 13) +1;
+	$("#Turn").html("<img src=\"../CSS/Image/"+type1 + "_" + couleur1 + ".jpg\" alt=\"...\" class=\"img-rounded\" height=\"120\" width=\"87\">");
+}
+
+River1.onclick=function(){
+	var couleur1 = Math.floor(Math.random() *4) +1;
+	var type1 = Math.floor(Math.random() * 13) +1;
+	$("#River").html("<img src=\"../CSS/Image/"+type1 + "_" + couleur1 + ".jpg\" alt=\"...\" class=\"img-rounded\" height=\"120\" width=\"87\">");
+}
