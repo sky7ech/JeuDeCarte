5ed78e037d6dbf6e65056b1153edfc84b709d22d
Gautier Delplace <gautier.delplace@hotmail.fr>
2015-03-23 16:53:54 +0100

    PageCreerCompte

--- src/main/webapp/JavaScript/GestionCompte.js
+++ src/main/webapp/JavaScript/GestionCompte.js
@@ -1,31 +1,66 @@
 $('#CreationCompte').click(function (event) {
-	if($('#NewPseudo').val() == $('#VerifMDP').val()){
-		$.ajax({
-			url: "/webapi/player",
-			contentType : 'application/json',
-			data: JSON.stringify({
-				pseudo: $('#NewPseudo').val(),
-				mdp: $('#NewMDP').val(),
-				nom : $('#NewNom').val(),
-				prenom : $('#NewPrenom').val(),
-			}),
-			type: 'POST',
-			dataType : "json",
-			success:function(json) {
-				$("#output").append("Compte Creer"); 
-			},
-			error: function( xhr, status, errorThrown ) {
-				alert( "Sorry, there was a problem!" );
-				console.log( "Error: " + errorThrown );
-				console.log( "Status: " + status );
-				console.dir( xhr );
-			},
-			complete: function( xhr, status ) {
-				alert( "The request is complete!" );
-			}
-		});
+	if($('#NewPseudo').val() != '' && $('#NewMDP').val() != '' &&  $('#NewNom').val() != '' && $('#NewPrenom').val()!= ''){
+		if($("#NewMDP").val() == $("#VerifMDP").val()){
+			$.ajax({
+				url: "/webapi/player",
+				contentType : 'application/json',
+				data: JSON.stringify({
+					pseudo: $('#NewPseudo').val(),
+					mdp: $('#NewMDP').val(),
+					nom : $('#NewNom').val(),
+					prenom : $('#NewPrenom').val(),
+				}),
+				type: 'POST',
+				dataType : "json",
+				success:function(json) {
+					$("#output").append("Compte Creer"); 
+				},
+				error: function( xhr, status, errorThrown ) {
+					alert( "Sorry, there was a problem!" );
+					console.log( "Error: " + errorThrown );
+					console.log( "Status: " + status );
+					console.dir( xhr );
+				},
+				complete: function( xhr, status ) {
+					alert( "The request is complete!" );
+				}
+			});
+		}
+		else {
+			$("#output").text("Mot de passe different");
+		}
 	}
-	else {
-		$("#output").append("Mot de passe different");
+	else{
+		$("#output").text("Champ vide")
 	}
+});
+
+PageConnexion.onclick = function(){
+	window.location.href = "Connexion.html";
+}
+
+$('#Connexion').click(function (event) {
+	$.ajax({
+		url: "/webapi/player/" + $('#pseudo').val() ,
+		contentType : 'application/json',
+		data: JSON.stringify({
+			pseudo : $('#pseudo').val(),
+			mdp : $('#MDP').val(),
+			
+		}),
+		type: 'GET',
+		dataType : "json",
+		success:function(json) {
+			window.location.href = "EcranJeu.html";
+		},
+		error: function( xhr, status, errorThrown ) {
+			alert( "Sorry, there was a problem!" );
+			console.log( "Error: " + errorThrown );
+			console.log( "Status: " + status );
+			console.dir( xhr );
+		},
+		complete: function( xhr, status ) {
+			alert( "The request is complete!" );
+		}
 	});
+});
